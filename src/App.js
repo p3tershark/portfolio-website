0c514155dd4394a998dc7742ba909f1e3b278816
WampirNinja <47037440+WampirNinja@users.noreply.github.com>
2020-07-17 13:12:28 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 
 import Home from "./components/pages/Home";
